73cac24976ffc908d28b39197c20a3d7f9b6b0f8
jaro124 <jaro124@gmail.com>
2024-03-19 14:03:30 +0100

    added code syntax highlighting

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -3,6 +3,20 @@ import { graphql } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/core/layout";
 import Seo from "../../components/core/seo";
+import {
+  Paragraf,
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  NumberedList,
+  DiscList,
+  MyBlockquote,
+  MyPRE,
+} from "../../components/core/layout-mdx.js";
+import { MDXProvider } from "@mdx-js/react";
 
 const BlogPost = ({ data, children }) => {
   const image = getImage(data.mdx.frontmatter.hero_image);
@@ -26,7 +40,23 @@ const BlogPost = ({ data, children }) => {
           {data.mdx.frontmatter.hero_image_credit_text}
         </a>
       </p>
-          {children}
+      <MDXProvider
+              components={{
+                p: Paragraf,
+                h1: H1,
+                h2: H2,
+                h3: H3,
+                h4: H4,
+                h5: H5,
+                h6: H6,
+                ol: NumberedList,
+                ul: DiscList,
+                blockquote: MyBlockquote,
+                pre: MyPRE,
+              }}
+            >
+              {children}
+            </MDXProvider>
         </div>
       </div>
     </Layout>
